4ab30971f17ecfd55898a67fc5b7a288007d097f
Brayanstyd <brayantriana22@gmail.com>
2021-06-02 10:00:36 -0500

    Corrección de cron job y pruebas ejecutadas

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        $schedule->command('activate:users')->everyFiveMinute();
+        $schedule->command('activate:users')->everyFiveMinutes();
     }
 
     /**
