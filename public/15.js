d81334da9153db4c614097651cb52c0bacfc1682
Brayanstyd <brayantriana22@gmail.com>
2021-07-29 12:53:43 -0500

    pd de chat, mensajes y foro, editado

--- public/15.js
+++ public/15.js
@@ -555,7 +555,7 @@ var render = function() {
             : _vm._e(),
           _vm._v(" "),
           _c("div", { staticClass: "row" }, [
-            _c("div", { staticClass: "col-md-2 pd-20 flotante" }, [
+            _c("div", { staticClass: "col-md-2 pd-18 flotante" }, [
               _c(
                 "button",
                 {
